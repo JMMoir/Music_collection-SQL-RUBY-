1748d892aed8db10ad1e0d21aa78fbe914e87a59
JMMoir <jmmoir@outlook.com>
2019-04-03 15:03:05 +0100

    created tables and sql_runnner

---
+++ db/music_collection.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists(
+  id SERIAL PRIMARY KEY,
+  artist_name VARCHAR
+);
+
+CREATE TABLE albums(
+  id SERIAL PRIMARY KEY,
+  album_name VARCHAR,
+  artist_id INT REFERENCES artists(id)
+);
---
+++ db/sql_runner.rb
@@ -0,0 +1,20 @@
+require('pg')
+
+class SqlRunner
+
+  def SqlRunner.run(sql, values =[])
+    #connect to db
+    begin
+      db = PG.connect({dbname: music_collection,
+                       host: 'localhost'})
+    #prepare statement
+    db.prepare("query", sql)
+    #exec prepare statement
+    result = db.exec_prepared("query", values)
+    #close db connection
+  ensure
+    db.close()
+  end
+  return result
+  end
+end
